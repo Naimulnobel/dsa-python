d73af878d50c26d3042b8447e8a91fce18843403
nobel <niloy.riseuplabs@gmail.com>
2024-06-20 16:55:09 +0600

    Update accessingList.py

--- list/accessingList.py
+++ list/accessingList.py
@@ -3,3 +3,9 @@ print(shoppingList[0])
 print('apple' in shoppingList)
 print(shoppingList[-1])
 print(shoppingList[::-1])
+for item in shoppingList:
+    print(item)
+print(len(shoppingList))
+
+for i in range(len(shoppingList)):
+    print(shoppingList[i])
