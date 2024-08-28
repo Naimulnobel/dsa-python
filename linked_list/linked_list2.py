150509e640dd706f7914b44d0cbd5f2986308118
nobel <nobel.naimul.riseuplabs@gmail.com>
2024-08-28 16:31:07 +0600

    Create linked_list2.py

---
+++ linked_list/linked_list2.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.length=0
+
+    def __str__(self):
+        temp_node=self.head
+        result=''
+        while temp_node is not None:
+            result+=str(temp_node.value)
+            if temp_node.next is not None:
+                result+='->'
+            temp_node=temp_node.next
+        return result
+    def append(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.length+=1
+        return self
+    def prepend(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+        self.length+=1
+        return self
+    def insert(self,index,value):
+        new_node=Node(value)
+        temp_node=self.head
+        if index==0:
+            self.prepend(value)
+        elif index>=self.length:
+            self.append(value)
+        else:
+            for _ in range(index-1):
+                temp_node=temp_node.next
+            new_node.next=temp_node.next
+            temp_node.next=new_node
+        self.length+=1
+        return self
+        
+new_list=LinkedList()
+new_list.append(1)
+print(new_list)
+new_list.prepend(0)
+print(new_list)
+new_list.insert(1,2)
+print(new_list)
+            
